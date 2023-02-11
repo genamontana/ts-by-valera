b458a77cce4203ee0d304588d3321f40190a730b
genamontana <predator199@yandex.ru>
2023-02-11 21:48:31 +0300

    25 min

--- src/state.ts
+++ src/state.ts
@@ -2,5 +2,8 @@ let a: number = 10
 let b: string = 'hello'
 let c: boolean = true
 
+let ages: number[] = [18, 12, 19, 22, 45];
+let ages1: Array<number> = [18, 12, 19, 22, 45];
+
 
 export default 1
