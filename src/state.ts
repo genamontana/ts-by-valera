09e3170ac1ec77bd533d65235744aa74e339ecbc
genamontana <predator199@yandex.ru>
2023-02-11 22:17:00 +0300

    55 min

--- src/state.ts
+++ src/state.ts
@@ -6,7 +6,7 @@ let c: boolean = true
 let ages: (number | string)[] = [18, '12', 19, 22, 45];
 let ages1: Array<number | string | boolean> = [18, '12', 19, 22, true];
 */
-interface Man1Type {
+interface IMan1Type {
     name: string
     height: number
 }
@@ -17,9 +17,19 @@ type CarType = {
 }
 
 
-let man1: Man1Type = {name: 'Dima', height: 1.78}
-let man2: Man1Type = {name: 'Sasha', height: 1.8}
+let man1: IMan1Type = {name: 'Dima', height: 1.78}
+let man2: IMan1Type = {name: 'Sasha', height: 1.8}
 let car: CarType = {model: 'Reno ', year: 2016}
+
+type Man3 = {
+    name: string
+    height: number
+}
+
+let man3: Man3[] = [
+    {name: 'Dima', height: 1.78},
+    {name: 'Sasha', height: 1.8}
+]
 
 
 export default 1
