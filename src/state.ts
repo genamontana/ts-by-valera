52880e52bcaa2b9345e3fa603c75c7fb14e44d8e
genamontana <predator199@yandex.ru>
2023-02-11 22:05:43 +0300

    40 min

--- src/state.ts
+++ src/state.ts
@@ -1,9 +1,25 @@
+/*
 let a: number = 10
 let b: string = 'hello'
 let c: boolean = true
 
-let ages: number[] = [18, 12, 19, 22, 45];
-let ages1: Array<number> = [18, 12, 19, 22, 45];
+let ages: (number | string)[] = [18, '12', 19, 22, 45];
+let ages1: Array<number | string | boolean> = [18, '12', 19, 22, true];
+*/
+interface Man1Type {
+    name: string
+    height: number
+}
+
+type CarType = {
+    model: string
+    year: number
+}
+
+
+let man1: Man1Type = {name: 'Dima', height: 1.78}
+let man2: Man1Type = {name: 'Sasha', height: 1.8}
+let car: CarType = {model: 'Reno ', year: 2016}
 
 
 export default 1
